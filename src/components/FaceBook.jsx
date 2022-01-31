01175db9c00777904f35a6065b3bc93bf1a9e725
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-31 10:18:19 +0100

    iteration 14, fixed filter by countries

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -5,16 +5,34 @@ const FaceBook = () => {
   const [users, setUsers] = useState(profiles);
   const [isClicked, setClick] = useState(false);
 
+  const showCountries = () => {
+    const arr = [];
+    profiles
+      .map((e) => e.country)
+      .forEach((e) => {
+        if (!arr.includes(e)) {
+          arr.push(e);
+        }
+      });
+    return arr;
+    // console.log('country :', arr);
+  };
+
   const handleClick = (country) => {
     country === ''
       ? setUsers(profiles)
       : setUsers(profiles.filter((e) => e.country === country));
   };
 
+  const countries = showCountries();
+
   return (
     <div>
       <button onClick={() => handleClick('')}>All</button>
-      <button></button>
+      {countries.map((e) => {
+        return <button onClick={() => handleClick(e)}>{e}</button>;
+      })}
+
       {users.map((e, index) => {
         return (
           <div key={index} className="card" style={{ marginBottom: 20 }}>
